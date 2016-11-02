1725d47acde2bdfde2cbedd1206007ce5ccee31e
Drew Seward <awswrd@gmail.com>
2016-11-01 20:45:40 -0700

    Initial commit
    Just a basic dumb web api project thing.

---
+++ HelloDockerOnWindows/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HelloDockerOnWindows.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ HelloDockerOnWindows/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HelloDockerOnWindows.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
